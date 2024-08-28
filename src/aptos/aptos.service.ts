f514c5e12a17e6c3f047bbf4a9df2461410c50af
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-08-28 21:32:51 +0530

    Custodial Account Created

--- src/aptos/aptos.service.ts
+++ src/aptos/aptos.service.ts
@@ -1,26 +1,34 @@
+import {
+  Account,
+  Aptos,
+  AptosConfig,
+  Ed25519Account,
+  Ed25519PrivateKey,
+  Network,
+} from '@aptos-labs/ts-sdk';
 import { Injectable } from '@nestjs/common';
-import { CreateAptoDto } from './dto/create-apto.dto';
-import { UpdateAptoDto } from './dto/update-apto.dto';
 
 @Injectable()
 export class AptosService {
-  create(createAptoDto: CreateAptoDto) {
-    return 'This action adds a new apto';
+  private readonly aptos: Aptos;
+  private readonly account: Ed25519Account;
+
+  constructor() {
+    const config = new AptosConfig({ network: Network.TESTNET });
+    this.aptos = new Aptos(config);
+
+    this.account = Account.fromPrivateKey({
+      privateKey: new Ed25519PrivateKey(process.env.APTOS_PRIVATE_KEY),
+    });
   }
 
-  findAll() {
-    return `This action returns all aptos`;
-  }
+  async getAccountData() {
+    const { accountAddress, privateKey, publicKey } = this.account;
 
-  findOne(id: number) {
-    return `This action returns a #${id} apto`;
-  }
-
-  update(id: number, updateAptoDto: UpdateAptoDto) {
-    return `This action updates a #${id} apto`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} apto`;
+    return {
+      accountAddress: accountAddress.toString(),
+      privateKey: privateKey.toString(),
+      publicKey: publicKey.toString(),
+    };
   }
 }
